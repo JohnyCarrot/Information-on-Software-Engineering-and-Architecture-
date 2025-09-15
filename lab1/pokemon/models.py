135492ccc7cecb74d2846bdb3814d13f120f7724
Roman Božik <web.kone@gmail.com>
2025-09-15 12:17:27 +0300

    Hello there

---
+++ lab1/pokemon/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Pokemon(models.Model):
+    TYPES = [
+        ('fire', 'Fire'),
+        ('water', 'Water'),
+        ('grass', 'Grass'),
+        ('electric', 'Electric'),
+        ('normal', 'Normal'),
+        ('rock', 'Rock'),
+    ]
+
+    name = models.CharField("Name",blank=True, null=True)
+    type = models.CharField("Type", choices=TYPES, blank=True, null=True)
+    level = models.IntegerField("Level", blank=True, null=True)
+    hp = models.IntegerField("HP", blank=True, null=True)
+    note = models.TextField("Note", blank=True, null=True)
+    image = models.ImageField("Image", upload_to="pokemons/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
